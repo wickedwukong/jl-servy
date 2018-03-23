661d9f5604c1a0cb34ad5baad147b25d903bb5a4
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-23 14:21:41 +0000

    get to the point where we can start next session

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -8,12 +8,12 @@ defmodule Servy.Handler do
 
   def parse(request) do
     # TODO: Parse the request string into a map:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+    conv = %{ method: "GET", path: "/users", resp_body: "" }
   end
 
   def route(conv) do
     # TODO: Create a new map that also has the response body:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+    conv = %{ method: "GET", path: "/users", resp_body: "Asad, Tom, Sri" }
   end
 
   def format_response(conv) do
@@ -23,14 +23,14 @@ defmodule Servy.Handler do
     Content-Type: text/html
     Content-Length: 20
 
-    Bears, Lions, Tigers
+    Asad, Tom, Sri
     """
   end
 
 end
 
 request = """
-GET /wildthings HTTP/1.1
+GET /users HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
